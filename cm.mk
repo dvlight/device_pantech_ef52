7c837a8512f71f9a9665f250bfe10d4dd3fa19f7
Dev-light1234 <shinsooan0610@gmail.com>
2019-11-01 16:08:58 +0900

    ef52: Rebrand aoscp -> aoscp && add fingerprint

--- cm.mk
+++ cm.mk
@@ -3,7 +3,9 @@ TARGET_SCREEN_HEIGHT := 1080
 TARGET_SCREEN_WIDTH := 720
 
 # Inherit some common BlurOS stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/aoscp/configs/common_full_phone.mk)
+
+-include vendor/cmsdk/Android.mk
 
 # Inherit device configuration
 $(call inherit-product, device/pantech/ef52/full_ef52.mk)
@@ -12,16 +14,16 @@ BOARD_HARDWARE_CLASS := device/pantech/ef52/lineagehw/
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := ef52
-PRODUCT_NAME := cm_ef52
+PRODUCT_NAME := aoscp_ef52
 PRODUCT_BRAND := Vega
 PRODUCT_MODEL := Vega Iron
 PRODUCT_MANUFACTURER := Vega
 
-# Set build fingerprint / ID / Product Name ect.
-#PRODUCT_BUILD_PROP_OVERRIDES += \
-#    PRODUCT_NAME="Vega Iron" \
-#    TARGET_DEVICE=ef52 \
-
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_DEVICE="ef52" \
+    PRODUCT_NAME="aoscp_ef52" \
+    BUILD_FINGERPRINT="pantech/ef52l:4.1.2/JZO54K/IM-A870L.003:user/release-keys" \
+    PRIVATE_BUILD_DESC="ef52l-user 4.1.2 JZ054K IM-A870L.003 release-keys"
 
 # Enable Torch
 #PRODUCT_PACKAGES += Torch
