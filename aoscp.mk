20ac275bcbbaf9d89d387eb0e789577da0ff6e0e
Dev-light1234 <shinsooan0610@gmail.com>
2019-11-02 22:14:57 +0900

    ef52: define fingerprint

--- aoscp.mk
+++ aoscp.mk
@@ -20,8 +20,9 @@ PRODUCT_MANUFACTURER := Vega
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="ef52" \
     PRODUCT_NAME="aoscp_ef52" \
-    BUILD_FINGERPRINT="pantech/ef52l:4.1.2/JZO54K/IM-A870L.003:user/release-keys" \
     PRIVATE_BUILD_DESC="ef52l-user 4.1.2 JZ054K IM-A870L.003 release-keys"
+
+BUILD_FINGERPRINT := pantech/ef52l:4.1.2/JZO54K/IM-A870L.003:user/release-keys
 
 # Enable Torch
 #PRODUCT_PACKAGES += Torch
