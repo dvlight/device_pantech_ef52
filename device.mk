f6f0a13dbb1565cb51c4673d54fd8f631d0e6dcb
Dev-light1234 <shinsooan0610@gmail.com>
2019-11-10 19:11:02 +0900

    ef52: camera upbring for AOSCP

--- device.mk
+++ device.mk
@@ -24,17 +24,13 @@ DEVICE_PACKAGE_OVERLAYS += device/pantech/ef52/overlay
 PRODUCT_AAPT_CONFIG := normal hdpi xhdpi
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
 
-# Camera
-#PRODUCT_PACKAGES += \
-#    camera.msm8960
+#Camero
+PRODUCT_PACKAGES += \
+    Camero
 
 # Lights
 #PRODUCT_PACKAGES += \
 	lights.msm8960
-
-# audio UCM files
-#PRODUCT_COPY_FILES += \
-                       device/pantech/ef52/snd_soc_msm/snd_soc_msm_2x_Fusion3:system/etc/snd_soc_msm/snd_soc_msm_2x_Fusion3 \
 
 # Checking model
 PRODUCT_COPY_FILES += \
